953ed28e50d94bf686c2570b5b9b8130cba4c9e9
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-02-22 21:57:33 -0300

    live

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/', (req, res, next) => res.send('vai vendo!!!'))
+app.use('/live', (req, res, next) => res.send('estou vivo!!!'))
 app.listen(port, () => console.log('estou aqui rodando o server'))
